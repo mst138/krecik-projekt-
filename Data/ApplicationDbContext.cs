d54b192fd9c0a5ef20ae4837375e48cf66ce2669
Marcin Mastalerz <mmastalerz138@gmail.com>
2021-03-04 17:11:35 +0100

    push

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using projekt.Models;
+
+namespace projekt.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<projekt.Models.Joke> Joke { get; set; }
+    }
+}
